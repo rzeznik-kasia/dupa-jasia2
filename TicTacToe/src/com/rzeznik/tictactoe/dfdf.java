93b2b32ca0fcdd238f802044f8e769fc0c7c34b8
rzeznik-kasia <rzeznik.kasia@gmail.com>
2016-11-03 22:34:01 +0100

    komit

--- TicTacToe/src/com/rzeznik/tictactoe/dfdf.java
+++ TicTacToe/src/com/rzeznik/tictactoe/dfdf.java
@@ -1,9 +1,12 @@
 package com.rzeznik.tictactoe;
 
 public class dfdf {
-///jakas klasa
+
+///jakas klasa? jakas zmiana mialo byc!!! robimy stasha?
 	///no a takie zmiany beda w drugim stashu?
+
 }
 
-// no i jak chce sobie zrobic trzecią zmiane
+// no i jak chce sobie zrobic trzecią zmiane 111111
 
+//
